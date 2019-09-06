1b2b2b12bf30f9b51aaf091bb0f051358c7c4a2e
Jake Johnson <jakejjohnson@gmail.com>
2019-09-06 11:53:51 -0500

    cards/index.js

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,77 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+const cardContainer = document.querySelector('.cards-container');
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+    .then((response) => {
+        //console.log(response);
+        console.log(response.data);
+        console.log(response.data.articles.bootstrap);
+        console.log(response.data.articles.javascript);
+        console.log(response.data.articles.jquery);
+        console.log(response.data.articles.node);
+        console.log(response.data.articles.technology);
+        bootstrapArray = response.data.articles.bootstrap;
+        jsArray = response.data.articles.javascript;
+        jqArray = response.data.articles.jquery;
+        nodeArray = response.data.articles.node;
+        techArray = response.data.articles.technology;
+
+        //bootstrapArray loop
+        for(i = 0; i < bootstrapArray.length; i++){
+            cardContainer.appendChild(makeCard(bootstrapArray[i]));
+        }
+        //jsArray loop
+        for(i = 0; i < jsArray.length; i++){
+            cardContainer.appendChild(makeCard(jsArray[i]));
+        }
+        //jqArray loop
+        for(i = 0; i < jqArray.length; i++){
+            cardContainer.appendChild(makeCard(jqArray[i]));
+        }
+        //nodeArray loop
+        for(i = 0; i < nodeArray.length; i++){
+            cardContainer.appendChild(makeCard(nodeArray[i]));
+        }
+        //techArray loop
+        for(i = 0; i < techArray.length; i++){
+            cardContainer.appendChild(makeCard(techArray[i]));
+        }        
+    })
+    .catch((error) => {
+        console.log(error);
+    })
+
+function makeCard(data) {
+    const card = document.createElement('div');
+    card.classList.add('card');
+
+    const headline = document.createElement('div')
+    headline.classList.add('headline');
+    
+    headline.textContent = data.headline;
+    card.appendChild(headline);
+
+    const author = document.createElement('div');
+    author.classList.add('author');
+    card.appendChild(author);
+
+    const imgContainer = document.createElement('div');
+    imgContainer.classList.add('img-container');    
+
+    const image = document.createElement('img');
+    image.src = data.authorPhoto;
+
+    const authorSpan = document.createElement('span');
+    authorSpan.textContent = data.authorName;
+
+    author.appendChild(imgContainer);
+    imgContainer.appendChild(image);    
+    
+    author.appendChild(authorSpan);
+    image.textContent = data.image;
+    authorSpan.textContent = data.authorName; 
+    return card;
+}
